6e265684d4bad393cf04141fdbe4700a4b93e3b9
azmec <pokekights6702@gmail.com>
2020-12-01 20:59:29 -0600

    Started rope code rework.

--- src/entities/rope-related/rope/rope.gd
+++ src/entities/rope-related/rope/rope.gd
@@ -8,27 +8,31 @@ const FRICTION = 2
 var total_rope_segments: float = 25.0
 var rope_segment_length: float = 4.0
 var max_length: float = 0.0
-var old_points: PoolVector2Array = []
 var collision_points: Array = []
+var segments: Array = []
 
 onready var timer: Timer = $Timer
 
 func _ready():
 	for unit in total_rope_segments:
-		add_point(Vector2(0, unit))
-		var new_shape = COLLISIONSHAPE.instance()
-		self.add_child(new_shape)
-		new_shape.set_index(unit)
-		new_shape.global_position = self.points[unit]
-		collision_points.append(new_shape)
-		self.points[0].y -= rope_segment_length
-	old_points = self.points
-	#print(self.points)
+		var position: Vector2 = Vector2(0, unit)
+		self.add_point(position)
+		var new_segment = SEGMENT.new()
+		segments.append(new_segment)
+		var new_collision_shape = COLLISIONSHAPE.instance()
+		collision_points.append(new_collision_shape)
+		new_segment.old_position = position
+		new_segment.new_position = position
+		self.add_child(new_collision_shape)
+		new_collision_shape.set_index(unit)
+		new_collision_shape.global_position = self.points[unit]
+		self.points[unit] = new_segment.new_position
+		self.points[0].y = self.points[0].y - rope_segment_length	
 
 func _process(delta: float) -> void:
-	self.points[0] = get_parent().get_child(0).global_position
+	self.points[0] = get_global_mouse_position()
 	# PERFORMING THE SIMULATION
-	_simulate(delta)
+	#_simulate(delta)
 	# CONSTRAINING THE SIMULATION
 	for i in range(0, 100):
 		_apply_constraints()
@@ -47,60 +51,40 @@ func get_simulated_rope_length() -> float:
 	return length
 
 func _simulate(delta: float) -> void:
-	for i in range(total_rope_segments + 1):
-		var first_segment_currently: Vector2 = self.points[i]
-		var first_segment_previously: Vector2 = self.old_points[i] 
-		var velocity: Vector2 = first_segment_currently - first_segment_previously
-
-		self.old_points[i] = first_segment_currently
+	for i in range(self.segments.size()):
+		var segment = self.segments[i]
+		var velocity: Vector2 = segment.new_position - segment.old_position
 		velocity.x = lerp(velocity.x, 0, FRICTION * delta)
-		first_segment_currently += velocity
-		first_segment_currently.y += GRAVITY * delta
-		if collision_points[i - 1].is_colliding():
-			first_segment_currently = first_segment_previously
-		self.points[i] = first_segment_currently
+		segment.new_position = segment.new_position + velocity
+		#segment.new_position.y = segment.new_position.y + GRAVITY * delta
+		if collision_points[i].is_colliding():
+			segment.new_position = segment.old_position
+		self.points[i] = segment.new_position
 	
 func _apply_constraints() -> void:
-	for i in range(self.points.size() - 1):
-		var first_segment: Vector2 = self.points[i]
-		var second_segment: Vector2 = points[i + 1]
-		collision_points[i].position = first_segment
-		var distance_between_points: float = first_segment.distance_to(second_segment)
-		# "error" is the difference or "extra length" between the above recorded distance
-		# between two distinct points and our purported "segment length"
-		# e.g. (A) if our segment length is 5, and the distance between two points is 10, we have
-		# an error of 5; (B) if our segment if 5, and the the distance between two points is 3, we
-		# have an error of 2
+	for i in range(self.segments.size() - 1):
+		var first_segment = self.segments[i]
+		var second_segment = self.segments[i + 1]
+		collision_points[i].position = first_segment.new_position
+		var distance_between_points: float = first_segment.new_position.distance_to(second_segment.new_position)
 		var error: float = abs(distance_between_points - rope_segment_length)
 
 		var new_direction: Vector2 = Vector2.ZERO
-		# case (A), the distance is greater than the given length, so we need a factor to move
-		# it IN by
 		if distance_between_points > rope_segment_length:
-			new_direction = (first_segment - second_segment).normalized()
-		# case (B), the distance is less than the given length, so we need a factor to move it
-		# OUT by
+			new_direction = (first_segment.new_position - second_segment.new_position).normalized()
 		else:
-			new_direction = (second_segment - first_segment).normalized()
+			new_direction = (second_segment.new_position - first_segment.new_position).normalized()
 		
-		# we now have a direction, so we multiply it by the error, yielding the point, *relative to
-		# a given point*, we need to move to
-		var distance_to_travel: Vector2 = new_direction * error		
-		# if it is NOT the first point of the rope, the "working end"	
+		var distance_to_travel: Vector2 = new_direction * error
 		if i != 0:
-			var first_current_position = first_segment
-			# we cut it in half, because we need to move both points to produce the required 
-			# segment length; in case (A) it would be 2.5, in case (B) it would be 1.5
-			first_current_position -= distance_to_travel * 0.5 
-			self.points[i] = first_current_position
-			var second_current_position = second_segment
-			second_current_position += distance_to_travel * 0.5
-			self.points[i + 1] = second_current_position
+			first_segment.new_position = first_segment.new_position - distance_to_travel * 0.5
+			second_segment.new_position = second_segment.new_position + distance_to_travel * 0.5
+			self.points[i] = first_segment.new_position
+			self.points[i + 1] = second_segment.new_position
 		else:
-			var second_current_position = second_segment
-			second_current_position += distance_to_travel
-			self.points[i + 1] = second_current_position
-		
+			second_segment.new_position = second_segment.new_position + distance_to_travel
+			self.points[i + 1] = second_segment.new_position
+
 
 
 
